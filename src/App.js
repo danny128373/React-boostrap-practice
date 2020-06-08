aba4edee6b701e2b7cf89863281020ed99964dd4
Daniel Meza <daniel.meza29@yahoo.com>
2020-06-07 22:00:56 -0500

    Added image background and added styling

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Contact from './Contact'
 import NoMatch from './NoMatch'
 import Layout from './components/Layout'
 import Navigation from './components/NavigationBar'
+import Jumbotron from './components/Jumbotron'
 
 export default function App() {
   return (
     <>
       <Navigation />
+      <Jumbotron />
       <Layout>
         <Switch>
           <Route exact path="/" component={Home} />
